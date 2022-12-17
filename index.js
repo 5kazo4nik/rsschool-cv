a90c87bb514784a1b33f4fd748f20da59cd84747
Oleg <alhimov.oleg2012@yandex.ru>
2022-12-17 19:43:35 +0300

    feat: add adaptation for mobile

--- index.js
+++ index.js
@@ -11,3 +11,31 @@
     }, 4000);
   });
 });
+
+const burgerBtn = document.querySelector('.burg-btn');
+
+const burgerEvent = (e) => {
+  if (e.target === burgerBtn || burgerBtn.contains(e.target)) {
+    document.querySelector('.burg-btn').classList.toggle('burg-btn-active');
+    document.querySelector('.nav-list').classList.toggle('nav-list-active');
+  }
+
+  const myInfo = burgerBtn.closest('.my-info');
+
+  document.body.addEventListener('click', (e) => {
+    if (!myInfo.contains(e.target)) {
+      document.querySelector('.burg-btn').classList.remove('burg-btn-active');
+      document.querySelector('.nav-list').classList.remove('nav-list-active');
+    }
+  });
+
+  const link = [...document.querySelectorAll('.nav-item a')];
+  link.forEach((el) =>
+    el.addEventListener('click', () => {
+      document.querySelector('.burg-btn').classList.remove('burg-btn-active');
+      document.querySelector('.nav-list').classList.remove('nav-list-active');
+    })
+  );
+};
+
+burgerBtn.addEventListener('click', burgerEvent);
